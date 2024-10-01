739ab2d8f73e9bd50859225bf0b6ab22ca234e1d
rzeradev <alephtus@gmail.com>
2024-10-01 13:17:44 -0300

    chore: Update .env.example and run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
+# Copy candidate-api/.env.example to candidate-api/.env
+cp candidate-api/.env.example candidate-api/.env
+
 # Start the containers
 docker compose up -d
 
 # Run migrations and install dependencies
 docker compose exec app composer install
 docker compose exec app php artisan migrate
+
+# Run laravel tests
+docker compose exec app php artisan test
